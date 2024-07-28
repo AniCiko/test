0ae6142f603ac926681d924bd6717a7429bf920f
AniCiko <anialesio1@gmail.com>
2024-07-28 11:09:02 +0200

    CreateApplicationStack

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("Juli", 23);
+            dict.Add("Envi", 24);
+            dict.Add("Alesio", 19);
+            dict.Add("Denis", 23);
+
+            foreach(var item in dict)
+            {
+                Console.WriteLine($"Emri: {item.Key}, Mosha: {item.Value}");
+            }
+        }
+    }
+}
